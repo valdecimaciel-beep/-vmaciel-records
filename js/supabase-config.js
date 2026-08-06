e22cb89c816abcd68804325b496c570cb2ccb89d
vmaciel <valdeci_maciel@hotmail.com>
2026-08-06 02:32:37 -0300

    Update Supabase anon key in supabase-config.js
    Updated the Supabase anon key in the configuration file.

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,6 +2,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const SUPABASE_URL = 'https://zufohisrbhrsqgtizlgq.supabase.co';
-const SUPABASE_ANON_KEY = 'COLE AQUI SUA ANON KEY';
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1Zm9oaXNyYmhyc2dxdGl6bGdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4ODg4MzksImV4cCI6MjA5OTQ2NDgzOX0.M6Gxd60jvxxJbtzC3dnk2UYqXNNVybb2kuQ1naP6c3g
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
