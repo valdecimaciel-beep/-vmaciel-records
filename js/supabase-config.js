15809b4918637c70603bed3f78e696d123a783e3
vmaciel <valdeci_maciel@hotmail.com>
2026-08-06 04:21:11 -0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,1 @@
 
-const SUPABASE_URL = 'https://zufohisrbhrsqgtizlgq.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1Zm9oaXNyYmhyc2dxdGl6bGdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4ODg4MzksImV4cCI6MjA5OTQ2NDgzOX0.M6Gxd60jvxxJbtzC3dnk2UYqXNNVybb2kuQ1naP6c3g';
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
