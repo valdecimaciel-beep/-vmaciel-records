a0fd3630bd81d426c340f2ea1391611333b8ca49
vmaciel <valdeci_maciel@hotmail.com>
2026-08-06 02:21:57 -0300

    Add Supabase client configuration

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,1 +1,7 @@
 
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+
+const SUPABASE_URL = 'https://zufohisrbhrsqgtizlgq.supabase.co';
+const SUPABASE_ANON_KEY = 'COLE AQUI SUA ANON KEY';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
